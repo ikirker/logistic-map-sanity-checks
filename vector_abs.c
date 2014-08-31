aad7497c2bb2687266f4f1998563fc5ff523a80c
Ian Kirker <ian.kirker@gmail.com>
2014-08-31 12:22:24 +0100

    Fixed accidental integer conversion

--- vector_abs.c
+++ vector_abs.c
@@ -1,10 +1,10 @@
 #include "vec_type.h"
-#include <stdlib.h>
+#include <math.h>
 
 void vector_abs(Vec_Type *v, int num_elements) {
     int i;
     for (i = 0; i < num_elements; i++) {
-        v[i] = abs(v[i]);
+        v[i] = fabs(v[i]);
     }
 }
 
